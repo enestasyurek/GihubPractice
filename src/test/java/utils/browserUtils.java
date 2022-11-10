f6b4da0248f6d7e1c0292662d697755136b105ef
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-11-10 15:20:39 +0300

    birthday

--- src/test/java/utils/browserUtils.java
+++ src/test/java/utils/browserUtils.java
@@ -2,7 +2,6 @@ package utils;
 
 public class browserUtils {
     public static void main(String[] args) {
-        System.out.println("İhsan is the best");
-        System.out.println("Enes is the best");
+        System.out.println("happy birthday");
     }
 }
